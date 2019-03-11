53d7c9b0810ab1756847fa06bab4e930f4aa59e9
oleg9952 <oleg.parhomenko2599@gmail.com>
2019-03-11 06:34:49 +0200

    js functions added to modify text

--- 6-lesson-text-editor/j.js
+++ 6-lesson-text-editor/j.js
@@ -2,7 +2,7 @@
 
 var titleEdit = document.querySelector('.title-edit');
 var articleTitle = document.querySelector('.title-article');
-var btn = document.querySelector('.fa-font');
+var btn = document.querySelector('.redir8');
 //function for setting custom title
 btn.addEventListener('click', function () {
    articleTitle.textContent = titleEdit.value;
@@ -17,12 +17,34 @@ titleEdit.addEventListener("keyup", function(event) {
   }
 });
 
+
 //*******************EDID-PANEL**************** */
+//main buttons
+/*
+var boldMain = document.querySelector('.fa-bold');
+var italicMain = document.querySelector('.fa-italic');
+var underlineMain = document.querySelector('.fa-underline');
+var justifyMain = document.querySelector('.fa-align-justify');
+var leftMain = document.querySelector('.fa-align-left');
+var centerMain = document.querySelector('.fa-align-center');
+var rightMain = document.querySelector('.fa-align-right');
+//redirectors
+var boldRedirect = document.querySelector('#redir1');
+var italicRedirect = document.querySelector('.redir2');
+var underlineRedirect = document.querySelector('.redir3');
+var justifyRedirect = document.querySelector('.redir4');
+var leftRedirect = document.querySelector('.redir5');
+var centerRedirect = document.querySelector('.redir6');
+var rightRedirect = document.querySelector('.redir7');
+
+boldRedirect.addEventListener('mouseover', function () {
+    boldMain.classList.toggle('active');
+    
+})
+*/
 
 
-left.addEventListener('click', function () {
-   right.click(); 
-});
+
 
 //shift+r reset styles
 //https://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
@@ -36,8 +58,7 @@ window.onkeypress = function(event) {
 
 
 
-
-function add_tags(x) {
+ function add_tags(x) {
     var z = x.toString()
     document.execCommand(z);
 }
