00c2079df0706b37e3aba2a259abff439a6bfe1a
Oleg <oleg.parhomenko2599@gmail.com>
2019-03-12 19:53:16 +0200

    removed comments from j.js in Text-Editior

--- 6-lesson-text-editor/j.js
+++ 6-lesson-text-editor/j.js
@@ -17,34 +17,14 @@ titleEdit.addEventListener("keyup", function(event) {
   }
 });
 
+ function add_tags(x) {
+    var z = x.toString()
+    document.execCommand(z);
+}
 
-//*******************EDID-PANEL**************** */
-//main buttons
-/*
-var boldMain = document.querySelector('.fa-bold');
-var italicMain = document.querySelector('.fa-italic');
-var underlineMain = document.querySelector('.fa-underline');
-var justifyMain = document.querySelector('.fa-align-justify');
-var leftMain = document.querySelector('.fa-align-left');
-var centerMain = document.querySelector('.fa-align-center');
-var rightMain = document.querySelector('.fa-align-right');
-//redirectors
-var boldRedirect = document.querySelector('#redir1');
-var italicRedirect = document.querySelector('.redir2');
-var underlineRedirect = document.querySelector('.redir3');
-var justifyRedirect = document.querySelector('.redir4');
-var leftRedirect = document.querySelector('.redir5');
-var centerRedirect = document.querySelector('.redir6');
-var rightRedirect = document.querySelector('.redir7');
-
-boldRedirect.addEventListener('mouseover', function () {
-    boldMain.classList.toggle('active');
-    
-})
-*/
-
-
-
+function colorWhite() {
+    document.execCommand('foreColor', false, "#fff");
+}
 
 //shift+r reset styles
 //https://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
@@ -55,12 +35,3 @@ window.onkeypress = function(event) {
     }
  }
  */
-
- function add_tags(x) {
-    var z = x.toString()
-    document.execCommand(z);
-}
-
-function colorWhite() {
-    document.execCommand('foreColor', false, "#fff");
-}
