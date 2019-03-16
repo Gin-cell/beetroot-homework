d84e360f82a3c95878c0311201cd1c6b33acb1b1
oleg9952 <oleg.parhomenko2599@gmail.com>
2019-03-17 01:10:47 +0200

    lesson 8 event-template || commit 4

---
+++ 8-event-template/j.js
@@ -0,0 +1,48 @@
+var about = document.querySelector('.nav-holder ul li:nth-child(1) a');
+var jury = document.querySelector('.nav-holder ul li:nth-child(2) a');
+var competitors = document.querySelector('.nav-holder ul li:nth-child(3) a');
+var contact = document.querySelector('.nav-holder ul li:nth-child(4) a');
+
+window.addEventListener('click', function (x) {
+   if (x.target == about) {
+        about.style.color = '#e71cb4';
+        jury.style.color = '#000';
+        competitors.style.color = '#000';
+        contact.style.color = '#000';
+
+        about.style.borderBottom = '2px solid #e71cb4'; 
+        jury.style.borderBottom = 'none'; 
+        competitors.style.borderBottom = 'none'; 
+        contact.style.borderBottom = 'none'; 
+    } else if (x.target == jury) {
+        about.style.color = '#000';
+        jury.style.color = '#e71cb4';
+        competitors.style.color = '#000';
+        contact.style.color = '#000';
+
+        about.style.borderBottom = 'none'; 
+        jury.style.borderBottom = '2px solid #e71cb4'; 
+        competitors.style.borderBottom = 'none'; 
+        contact.style.borderBottom = 'none'; 
+    } else if (x.target == competitors) {
+        about.style.color = '#000';
+        jury.style.color = '#000';
+        competitors.style.color = '#e71cb4';
+        contact.style.color = '#000';
+
+        about.style.borderBottom = 'none'; 
+        jury.style.borderBottom = 'none'; 
+        competitors.style.borderBottom = '2px solid #e71cb4'; 
+        contact.style.borderBottom = 'none'; 
+    } else if (x.target == contact) {
+        about.style.color = '#000';
+        jury.style.color = '#000';
+        competitors.style.color = '#000';
+        contact.style.color = '#e71cb4';
+        
+        about.style.borderBottom = 'none'; 
+        jury.style.borderBottom = 'none'; 
+        competitors.style.borderBottom = 'none'; 
+        contact.style.borderBottom = '2px solid #e71cb4'; 
+    }
+});
