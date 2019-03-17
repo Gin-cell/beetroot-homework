7a7ba8e3e8332b6d2927ad3ab50b5ebf411832c7
oleg9952 <oleg.parhomenko2599@gmail.com>
2019-03-17 16:21:51 +0200

    lesson 8 event-template || commit 6

--- 8-event-template/j.js
+++ 8-event-template/j.js
@@ -6,38 +6,38 @@ var contact = document.querySelector('.nav-holder ul li:nth-child(4) a');
 window.addEventListener('click', function (x) {
    if (x.target == about) {
         about.style.color = '#e71cb4';
-        jury.style.color = '#000';
-        competitors.style.color = '#000';
-        contact.style.color = '#000';
+        jury.style.color = '#fff';
+        competitors.style.color = '#fff';
+        contact.style.color = '#fff';
 
         about.style.borderBottom = '2px solid #e71cb4'; 
         jury.style.borderBottom = 'none'; 
         competitors.style.borderBottom = 'none'; 
         contact.style.borderBottom = 'none'; 
     } else if (x.target == jury) {
-        about.style.color = '#000';
+        about.style.color = '#fff';
         jury.style.color = '#e71cb4';
-        competitors.style.color = '#000';
-        contact.style.color = '#000';
+        competitors.style.color = '#fff';
+        contact.style.color = '#fff';
 
         about.style.borderBottom = 'none'; 
         jury.style.borderBottom = '2px solid #e71cb4'; 
         competitors.style.borderBottom = 'none'; 
         contact.style.borderBottom = 'none'; 
     } else if (x.target == competitors) {
-        about.style.color = '#000';
-        jury.style.color = '#000';
+        about.style.color = '#fff';
+        jury.style.color = '#fff';
         competitors.style.color = '#e71cb4';
-        contact.style.color = '#000';
+        contact.style.color = '#fff';
 
         about.style.borderBottom = 'none'; 
         jury.style.borderBottom = 'none'; 
         competitors.style.borderBottom = '2px solid #e71cb4'; 
         contact.style.borderBottom = 'none'; 
     } else if (x.target == contact) {
-        about.style.color = '#000';
-        jury.style.color = '#000';
-        competitors.style.color = '#000';
+        about.style.color = '#fff';
+        jury.style.color = '#fff';
+        competitors.style.color = '#fff';
         contact.style.color = '#e71cb4';
         
         about.style.borderBottom = 'none'; 
