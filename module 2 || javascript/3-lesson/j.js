c575bf0065dbf25d4e5a860e26d4c9b04a22bcc4
oleg9952 <oleg.parhomenko2599@gmail.com>
2019-05-15 20:39:49 +0300

    Lesson 3 DOM-list || completed

---
+++ module 2 || javascript/3-lesson/j.js
@@ -0,0 +1,39 @@
+var listItem = document.querySelector('.list');
+var playlist = [
+    {
+        author: "LED ZEPPELIN",
+        song:"STAIRWAY TO HEAVEN"
+    },
+    {
+        author: "QUEEN",
+        song:"BOHEMIAN RHAPSODY"
+    },
+    {
+        author: "LYNYRD SKYNYRD",
+        song:"FREE BIRD"
+    },
+    {
+        author: "DEEP PURPLE",
+        song:"SMOKE ON THE WATER"
+    },
+    {
+        author: "JIMI HENDRIX",
+        song:"ALL ALONG THE WATCHTOWER"
+    },
+    {
+        author: "AC/DC",
+        song:"BACK IN BLACK"
+    },
+    {
+        author: "QUEEN",
+        song:"WE WILL ROCK YOU"
+    },
+    {
+        author: "METALLICA",
+        song:"ENTER SANDMAN"
+    }
+];
+
+playlist.forEach(x => {
+    listItem.innerHTML += `<li>${x.author}, ${x.song};</li>`;
+});
