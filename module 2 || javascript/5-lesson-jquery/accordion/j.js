83e5e50fd0690b4109baea7336c5cd0f808ca97c
oleg9952 <oleg.parhomenko2599@gmail.com>
2019-05-25 01:35:51 +0300

    Lesson 5 || jQuery || Accordion completed

---
+++ module 2 || javascript/5-lesson-jquery/accordion/j.js
@@ -0,0 +1,17 @@
+$(function () {
+   $('.location').click(function () {
+      $('.location + div').slideToggle();      
+   });
+   $('.music').click(function () {
+      $('.music + div').slideToggle();      
+   });
+   $('.notes').click(function () {
+      $('.notes + div').slideToggle();      
+   });
+   $('.books').click(function () {
+      $('.books + div').slideToggle();      
+   });
+   $('.tendances').click(function () {
+      $('.tendances + div').slideToggle();      
+   });
+});
