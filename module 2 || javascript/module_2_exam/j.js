89ccd8fa2952ee412446551d522f34450971512e
Oleg <oleg.parhomenko2599@gmail.com>
2019-06-09 19:30:43 +0300

    Module 2 || Exam || commit 5

--- module 2 || javascript/module_2_exam/j.js
+++ module 2 || javascript/module_2_exam/j.js
@@ -1,3 +1,44 @@
+//---------modals---------
+var modalBG = document.querySelector('.modals_bg');
+var trade_modal = document.querySelector('#modal-content');
+var btn = document.querySelector('#btn');
+var closeBtn = document.querySelector('.closeBtn');
+
+btn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+function openModal() {
+   modal.style.display = 'block';
+   modal.style.animationName = '';
+   modalContent.style.animationName = '';
+}
+
+function closeModal() {
+  //modal.style.display = 'none';
+  modal.style.animationName = 'fadeOut';
+  modal.style.animationDuration = '800ms';
+  modalContent.style.animationName = 'hide';
+  modalContent.style.animationDuration = '1s';
+  setTimeout(close, 800);
+}
+
+function outsideClick(e) {
+  if (e.target == modal) {
+    //modal.style.display = 'none';
+    modal.style.animationName = 'fadeOut';
+  	modal.style.animationDuration = '800ms';
+  	modalContent.style.animationName = 'hide';
+  	modalContent.style.animationDuration = '1s';
+  	setTimeout(close, 800);
+  }
+}
+
+function close() {
+	modal.style.display = 'none';
+}
+
+
 //---------google_maps---------
 (function($) {
     $(window).on('load', function () {
@@ -25,14 +66,23 @@
  })(jQuery);
 
  //---------slick_slider---------
+ $('.slider_holder').slick({
+   dots: true,
+   infinite: true,
+   speed: 500,  
+   autoplay: true,
+   autoplaySpeed: 4000
+ });
+
+
  $('.news_gallery').slick({
     dots: true,
     infinite: true,
     speed: 600,
     slidesToShow: 3,
     adaptiveHeight: true,
-   //  autoplay: true,
-   //  autoplaySpeed: 2000
+   //autoplay: true,
+   //autoplaySpeed: 2000
   });
 
 //---------footer_form---------
